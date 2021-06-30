84f2a2337dfc63c7b1d7a2dfe0ed97a5105a4e56
Suyash S <suyash988@gmail.com>
2021-06-29 20:09:36 -0400

    layout added

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-
-const API_KEY =  "568ED056-CB1F-4F76-B357-80A2600EF0BF";
+const API_KEY = '568ED056-CB1F-4F76-B357-80A2600EF0BF';
 const BASE_URL = 'https://rest.coinapi.io';
 
 const get_datas = () => {
-    fetch(`${BASE_URL}/v1/assets`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CoinAPI-Key": API_KEY,
-            'Access-Control-Allow-Origin': '*'
-        }
-    }).then((response) => {
-            if (response.ok) {
-                response.json().then((json) => console.log(json))
-            }
-        }).catch(error => {
-            console.log('error', error);
-        })
+  fetch(`${BASE_URL}/v1/assets`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CoinAPI-Key': API_KEY,
+      'Access-Control-Allow-Origin': '*',
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((json) => console.log(json));
+      }
+    })
+    .catch((error) => {
+      console.log('error', error);
+    });
+};
 
-}
-
-get_cryptos()
+// get_datas();
