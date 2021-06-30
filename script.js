61e785be7e1dc6b46769722b81662d01315dee70
Saka Ricky <sakaricky91@gmail.com>
2021-06-30 22:33:05 +0100

    Added the HTML to display the data

--- script.js
+++ script.js
@@ -29,9 +29,28 @@ const get_datas = async () => {
     return curr.type_is_crypto && curr.price_usd;
   });
   // Sort array of objects by price_usd
-  const sortedOnlyCrypto = onlyCrypto.sort((first, second) => first.price_usd - second.price_usd)
+  const sortedOnlyCrypto = onlyCrypto.sort((first, second) => second.price_usd - first.price_usd)
   sortedOnlyCrypto.forEach(item => console.log(item));
-  
+
+  // The name tells it all
+  const number_of_cryptos_to_display = 5;
+  const cryptos_to_display = sortedOnlyCrypto.slice(0, number_of_cryptos_to_display);
+
+  // variable to hold the HTML to be added to the DOM
+  let table_cryptos = '';
+  cryptos_to_display.forEach(crypto => {
+    table_cryptos += '<tr>';
+    table_cryptos += `<td> ${crypto.name} </td>`;
+    table_cryptos += `<td> ${crypto.asset_id} </td>`;
+    table_cryptos += `<td> ${crypto.price_usd.toFixed(1)} </td>`;
+    table_cryptos += `<td> ${Number(crypto.volume_1day_usd.toExponential()).toPrecision(2)} </td>`;
+    table_cryptos += `<td> ${crypto.price_usd.toFixed(1)} </td>`; // API doesn't ruturn market cap
+    table_cryptos += `<td> NOT AVAILABLE </td>`;
+    table_cryptos += `<td> <3 </td></tr>`;
+  })
+
+  document.getElementById('cryptos_table').innerHTML = table_cryptos;
+
 };
 
 get_datas();
