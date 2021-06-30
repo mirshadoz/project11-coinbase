06773f42f5d985ce31ef82432e0fb5a627355da8
Saka Ricky <sakaricky91@gmail.com>
2021-06-30 16:00:08 +0100

    Display some cryptos in the HTML table. Sorting not working properly though

--- script.js
+++ script.js
@@ -12,7 +12,29 @@ const get_datas = () => {
   })
     .then((response) => {
       if (response.ok) {
-        response.json().then((json) => console.log(json));
+        response.json().then((json) => {
+          const datas = json
+          const cryptos = datas.filter(data => data.type_is_crypto)
+          const sorted_cryptos = [...cryptos] // Do a copy because sort() affects in place
+          sorted_cryptos.sort((crypto_1, crypto_2) => {
+            return crypto_2.price_usd - crypto_1.price_usd
+          })
+          const cryptos_to_display = sorted_cryptos.slice(0, 5);
+
+          let table_cryptos = ''
+          cryptos_to_display.forEach(crypto => {
+            table_cryptos += "<tr>";
+            table_cryptos += `<td> ${crypto.name} </td>`;
+            table_cryptos += `<td> ${crypto.asset_id} </td>`;
+            table_cryptos += `<td> ${crypto.price_usd.toFixed(1)} </td>`;
+            table_cryptos += `<td> ${Number(crypto.volume_1day_usd.toExponential()).toPrecision(2)} </td>`;
+            table_cryptos += `<td> ${crypto.price_usd.toFixed(1)} </td>`; // API doesn't ruturn market cap
+            table_cryptos += `<td> NOT AVAILABLE</td>`;
+            table_cryptos += `<td><3</td></tr>`;
+          })
+          document.getElementById('table_cryptos').innerHTML = table_cryptos
+          console.log('sorted_cryptos', sorted_cryptos);
+        });
       }
     })
     .catch((error) => {
@@ -20,4 +42,4 @@ const get_datas = () => {
     });
 };
 
-// get_datas();
+get_datas();
